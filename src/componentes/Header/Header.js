257a51687ddb5c77ad0a9eebbd5f0b2500a18169
EsneyderVega <kisabella2016@gmail.com>
2023-04-19 20:26:56 -0500

    actualizacion header

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,7 +1,7 @@
 import "./Header.css"
 function Header(){
     return <header className="header">
-        <img src='/img/header.png' alt='Org'/>
+        <img src='https://github.com/EsneyderVega/React/blob/main/public/img/Header.png?raw=true' alt='Org'/>
     </header>
 }
 
